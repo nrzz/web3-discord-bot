7ba4e53b3881a15acd48bc2511d228f1fb85a655
Fain17 <chetr0947@gmail.com>
2025-07-03 12:38:59 +0530

    Added linting and trivy steps

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],
+    'no-console': 'warn',
+    'prefer-const': 'error',
+    'no-var': 'error'
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'logs/',
+    '*.log'
+  ]
+}; 
